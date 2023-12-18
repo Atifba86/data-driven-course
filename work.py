e12e0d5ce141d18fa8deaf36a78ee8657ff00887
Rao Atif <ratifba86@gmail.com>
2023-12-18 10:23:52 +0500

    18-Dec-2023

---
+++ work.py
@@ -0,0 +1,13 @@
+import requests 
+from bs4 import BeautifulSoup
+import lxml
+
+url = 'https://examadmin.icaponline.net.pk:8888/ords/r/liveapp/100/files/static'
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
+
+print(soup.prettify)
+
+
